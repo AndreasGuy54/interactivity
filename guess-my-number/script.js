3b833a620246dc2d9d3c80e30b957208b95cb906
Andreas Duke Abaidoo <abaidooandy@gmail.com>
2021-12-05 16:58:35 +0000

    Handling Click Events for no input

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -1,10 +1,9 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value;
-document.querySelector('.guess').value = 23;
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔No number!';
+  }
+});
